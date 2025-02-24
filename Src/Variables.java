36776dec0275297ba1dc83d9c3177a9b224d77ef
jainkavya <jkavya865@gmail.com>
2025-02-25 03:12:48 +0530

    variables

--- Src/Variables.java
+++ Src/Variables.java
@@ -9,5 +9,23 @@ public class Variables {
         // reference = memory address (stack) that points to the (heap)
 
         // Primitive vs Reference variables
+        // ---------    -------------------
+        // int           string 
+        // double        array
+        // char          object
+        // boolean       
+        
+        // 2 steps to creating a variable 
+        // ----------------------------
+        // 1. declaration 
+        // 2. assignment 
+
+        int age = 19;
+        int year = 2025;
+        int quantity = 5;
+
+        System.out.println(age);
+        System.out.println("The year is " + year);
+        System.out.println(quantity);
     }
 }
