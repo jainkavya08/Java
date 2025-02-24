ab681cdb9784bf2339bca7e637678e9f980efbf0
jainkavya <jkavya865@gmail.com>
2025-02-24 22:51:53 +0530

    mon

--- Src/Main.java
+++ Src/Main.java
@@ -1,8 +1,20 @@
 public class Main {
     public static void main (String[] args) 
     {
+
+        // THis is java programming language 
+        
+        /*
+         * This a multi 
+         * line 
+         * comment
+         */
+
+
         System.out.println("I like pizza !");
         System.out.println("I want pizza !");
         System.out.println("I buy pizza !");
     }
 }
+
+
---
+++ Src/Variables.java
@@ -0,0 +1,13 @@
+public class Variables {
+    
+    public static void main (String[] args) 
+    {
+        // variable = a reusable container for a value 
+        //              a variable behaves as if it was the value it contains 
+
+        // Primitive = simple value is stored directly in memory (stack)
+        // reference = memory address (stack) that points to the (heap)
+
+        // Primitive vs Reference variables
+    }
+}
