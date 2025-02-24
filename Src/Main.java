dc071efb2b1142b2d21b2a257a4473ee15073c9d
jainkavya <jkavya865@gmail.com>
2025-02-24 14:11:19 +0530

    Mon

--- Src/Main.java
+++ Src/Main.java
@@ -1,7 +1,8 @@
 public class Main {
-    public static void main (String[] args) {
-        System.out.println("I like pizza!");
-        System.out.println("I want pizza!");
-        System.out.println("Buy me a pizza!");
+    public static void main (String[] args) 
+    {
+        System.out.println("I like pizza !");
+        System.out.println("I want pizza !");
+        System.out.println("I buy pizza !");
     }
 }
